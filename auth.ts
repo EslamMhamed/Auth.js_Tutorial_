16134fa1a2a0e82ba8323e1b2eacc61c5ba69acd
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-05 14:19:25 +0200

    add adapter prisma

--- auth.ts
+++ auth.ts
@@ -1,21 +1,26 @@
+import prisma from "@/lib/prisma"
 import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 
 
-export const { auth, handlers, signIn, signOut } = NextAuth({ providers: [GitHub, 
+export const { auth, handlers, signIn, signOut } = NextAuth({ 
+    adapter : PrismaAdapter(prisma),
+    providers: [GitHub, 
     Credentials({
         credentials :{
             email: {},
             password : {}
         },
         authorize : async(credentials) => {
-            const email = "admin@admin.com"
-            const password = "1234"
-            if(credentials.email === email && credentials.password === password){
-                return {email, password}
-            }else{
+            const user = await prisma.user.findFirst({
+                where:{email: credentials.email as string, password: credentials.password as string}
+            })
+            if(!user){
                 throw new Error("Invalid credentials")
+            }else{
+                return user
             }
         }
     })

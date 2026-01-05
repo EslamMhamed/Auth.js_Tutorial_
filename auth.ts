fabfef0c65a5b3afe206c335ca0dee50e4892c89
Eslam AbdelWahab <mohaeslam22@gmail.com>
2026-01-05 15:08:22 +0200

    update

--- auth.ts
+++ auth.ts
@@ -1,27 +1,68 @@
-import prisma from "@/lib/prisma"
-import NextAuth from "next-auth"
-import Credentials from "next-auth/providers/credentials"
-import GitHub from "next-auth/providers/github"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prisma";
+import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { schema } from "@/lib/validation";
+import { v4 as uuid } from "uuid";
+import { encode as defaultEncode } from "next-auth/jwt";
 
-
-export const { auth, handlers, signIn, signOut } = NextAuth({ 
-    adapter : PrismaAdapter(prisma),
-    providers: [GitHub, 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub,
     Credentials({
-        credentials :{
-            email: {},
-            password : {}
-        },
-        authorize : async(credentials) => {
-            const user = await prisma.user.findFirst({
-                where:{email: credentials.email as string, password: credentials.password as string}
-            })
-            if(!user){
-                throw new Error("Invalid credentials")
-            }else{
-                return user
-            }
+      credentials: {
+        email: {},
+        password: {},
+      },
+      authorize: async (credentials) => {
+        const validatedCredentials = schema.parse(credentials);
+        const user = await prisma.user.findFirst({
+          where: {
+            email: validatedCredentials.email as string,
+            password: validatedCredentials.password as string,
+          },
+        });
+        if (!user) {
+          throw new Error("Invalid credentials");
+        } else {
+          return user;
         }
-    })
-] })
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account?.provider === "credentials") {
+        token.credentials = true
+
+      }
+      return token
+    },
+  },
+  jwt: {
+    encode: async function (params) {
+      if (params.token?.credentials) {
+        const sessionToken = uuid();
+
+        if (!params.token.sub) {
+          throw new Error("No user ID found in token");
+        }
+
+        const createdSession = await PrismaAdapter(prisma)?.createSession?.({
+          sessionToken: sessionToken,
+          userId: params.token.sub,
+          expires: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000),
+        });
+
+        if (!createdSession) {
+          throw new Error("Failed to create session");
+        }
+
+        return sessionToken;
+      }
+      return defaultEncode(params);
+    },
+  },
+});
